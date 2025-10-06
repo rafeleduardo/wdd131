bdd710c4763ef3fd350c955c1be9fe11591559c8
Rafael Parra <rparra@byupathway.edu>
2025-10-07 01:27:31 +0200

    Product Review Form

---
+++ scripts/form.js
@@ -0,0 +1,39 @@
+﻿const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+const productNameSelect = document.getElementById('productName');
+
+products.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product.id;
+    option.textContent = product.name;
+    productNameSelect.appendChild(option);
+});
+
+document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
---
+++ scripts/review.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const reviewCountElement = document.getElementById('reviewCount');
+    let reviewCount = localStorage.getItem('reviewCount') || 0;
+    reviewCount = parseInt(reviewCount) + 1;
+    localStorage.setItem('reviewCount', reviewCount);
+    reviewCountElement.textContent = reviewCount;
+
+    document.getElementById('year').textContent = new Date().getFullYear();
+    document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+});
